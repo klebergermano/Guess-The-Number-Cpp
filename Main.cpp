7259d9b4a66fad5ea0beefa98b7c9a64716fa41c
Kleber Germano <kleber_lsgermano@hotmail.com>
2020-07-02 08:57:48 -0300

    Play Again added

--- Main.cpp
+++ Main.cpp
@@ -11,7 +11,6 @@ using std::string;
 string title();
 void playGame();
 
-
 int menu(int choice) {
 	if (choice == 0) {
 		return 0;
@@ -25,23 +24,20 @@ int main() {
 
 	//Title of the Game
 	cout << title();
-
-
 	
-	//Play Game
+	//Play Menu
 	cout << "\n";
-	cout << " 0. Quit the Game \n \n ";
-	cout << "1. Play the Game \n \n ";
+	cout << " [q] - Quit the Game \n \n ";
+	cout << "[s] - Start the Game \n \n ";
 
-	int option; 
+	string option; 
 	cin >> option;
-	if (option == 0) {
+
+	if (option == "q" || option == "Q") {
 		return 0; 
 	}
-	else if (option == 1) {
-		// Info Game
-		cout << "#### Guess The Number Game #### \n\n";
-		cout << "In this game you have to guess the random number between 1 and 100 \n\n";
+
+	else if (option == "s" || option == "S") {
 		playGame();
 	}
 	else {
--- playGame.cpp
+++ playGame.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 #include <string>
 #include <time.h>
+#include <vector>
 
 using std::cout;
 using std::cin;
 using std::endl; 
 using std::string;
+void playGame(); 
+string winAscii();
+string loseAscii(int rand_num);
 
-string loseAscii(int rand_num); 
+void playAgain() {
+	string play_again = "n";
+
+	cout << "You Wanna play again? \n \n";
+	cout << "[y] to YES \n \n";
+	cout << "[n] to NO \n \n";
+	cin >> play_again;
+
+	if (play_again == "y" || play_again == "Y") {
+		playGame();
+	}
+	else {
+		cout << "Tank you for play, see ya o/ \n \n";
+	}
+}
 
 int rand_number() {
 	int rand_num;
@@ -16,12 +34,11 @@ int rand_number() {
 	return rand_num;
 }
 
-void show_guesses(int arr_guesses[], int size) {
+void show_guesses(std::vector<int> arr_guesses) {
 	cout << "### ";
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < arr_guesses.size(); i++) {
 		if (arr_guesses[i] != 0) {
 			cout << arr_guesses[i] << " ";
-
 		}
 	}
 }
@@ -29,61 +46,55 @@ void show_guesses(int arr_guesses[], int size) {
 void playGame() {
 
 	int rand_num = rand_number();
+	cout << "The right number is: " << rand_num << "\n";
 	int guess;
 	int num_guesses = 0;
-	const int limit_guesses = 1;
-	int arr_guesses[limit_guesses] = {};
-	int arr_guesses_size = sizeof(arr_guesses) / sizeof(arr_guesses[0]);
+	const int limit_guesses = 5;
+	std::vector<int> arr_guesses;
 
-	cout << "You have " << limit_guesses << " chances\n\n";
+	// Info Game
+	cout << "#### Guess The Number Game #### \n \n";
+	cout << "In this game you have to guess the random number between 1 and 100 \n \n";
+	cout << "You have " << limit_guesses << " chances \n \n";
+
 	while (num_guesses < limit_guesses) {
 		cin >> guess;
-		// Store the guesses maked
-		arr_guesses[num_guesses] = guess;
+		// Store the guesses maked 
+		arr_guesses.push_back(guess);
 
 		//Win message
 		if (guess == rand_num) {
-			cout << R"(
-|||||||||||||||||||||||||||||||||||||||||||||||||||||
------------------------------------------------------
---------------------YOU WIN!!!-----------------------
------------------------------------------------------
-||||| CONGRATULATIONS YOU HIT THE RIGHT NUMBER ||||||
------------------------------------------------------
-|||||||||||||||||||||||||||||||||||||||||||||||||||||
-					  )";
-			cout << "\n\n";
+			
+			//win message
+			cout << winAscii(); 
+			cout << "\n \n";
+			
+			//playAgain
+			playAgain();
 			break;
 		}
 		else {
+	
 			if (guess > rand_num) {
-				cout << "Too high \n\n";
+				cout << "Too high \n \n";
 			}
 			else if (guess < rand_num) {
-				cout << "Too low \n\n";
+				cout << "Too low \n \n";
 			}
 			num_guesses++;
 		}
 	}
 
 	//Show guesses maked
-	show_guesses(arr_guesses, arr_guesses_size);
-
+	show_guesses(arr_guesses);
 
 	cout << "### \n";
-
 	// You Lose message
 	if (num_guesses >= limit_guesses) {
-
-		 
-
-
-	
 		cout << loseAscii(rand_num);
 
 		// Show the right number
-		cout << "\n" << "The right number was: " << rand_num;
-		return;
+		cout << "\n" << "The right number was: " << rand_num << "\n \n";
+		playAgain();
 	}
-
 }
